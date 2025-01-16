a7f2270475057b20de9191dfad8410268d8d9854
yangruitao <18728163854@163.com>
2025-01-16 14:40:38 +0800

    add includes

---
+++ library.cpp
@@ -0,0 +1,7 @@
+#include "library.h"
+#include "libfilezilla.hpp"
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef LIBFILEZILLA_LIBRARY_H
+#define LIBFILEZILLA_LIBRARY_H
+
+void hello();
+
+#endif //LIBFILEZILLA_LIBRARY_H
